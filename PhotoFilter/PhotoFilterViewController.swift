a812c941fe53ae7b533d662702c30edd87efe1f1
Vici Shaweddy <vshaweddy@gmail.com>
2020-01-14 22:38:58 -0500

    setting up

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -1,5 +1,5 @@
 import UIKit
-import CoreImage
+import CoreImage.CIFilterBuiltins
 import Photos
 
 class PhotoFilterViewController: UIViewController {
@@ -8,18 +8,59 @@ class PhotoFilterViewController: UIViewController {
 	@IBOutlet weak var contrastSlider: UISlider!
 	@IBOutlet weak var saturationSlider: UISlider!
 	@IBOutlet weak var imageView: UIImageView!
+    
+    var originalImage: UIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    
+    private let context = CIContext(options: nil)
+    private let filter = CIFilter(name: "CIColorControls")!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+        
+        originalImage = imageView.image
 
 	}
+    
+    private func image(byFiltering image: UIImage) -> UIImage {
+        // Do something interesting!
+        return image
+    }
+    
+    private func updateImage() {
+        if let originalImage = originalImage {
+            imageView.image = image(byFiltering: originalImage)
+        } else {
+            imageView.image = nil
+        }
+    }
 	
 	// MARK: Actions
 	
 	@IBAction func choosePhotoButtonPressed(_ sender: Any) {
 		// TODO: show the photo picker so we can choose on-device photos
+        presentImagePickerController()
 		// UIImagePickerController + Delegate
 	}
+    
+    private func presentImagePickerController() {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else
+        {
+            // make as an alert
+            print("The photo library isn't available.")
+            return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
 	
 	@IBAction func savePhotoButtonPressed(_ sender: UIButton) {
 		// TODO: Save to photo library
@@ -41,3 +82,22 @@ class PhotoFilterViewController: UIViewController {
 	}
 }
 
+
+extension PhotoFilterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // if user cancels the action so dismiss
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // check if there is original image or not
+        if let image = info[.editedImage] as? UIImage {
+            originalImage = image
+        } else if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
